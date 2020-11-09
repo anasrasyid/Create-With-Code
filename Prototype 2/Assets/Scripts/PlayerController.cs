e6fdd3505fbd2391cd3efdf33e38b2c9c3f9e42e
Anas Rasyid <anasrasyid333@gmail.com>
2020-11-10 06:57:57 +0700

    Move with Boundary

---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
+
+    private float inputHorizontal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check left boundary
+        if (transform.position.x < -xRange)
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+
+        // check right boundary
+        if (transform.position.x > xRange)
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+
+        // get player input and move the player
+        inputHorizontal = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * speed * inputHorizontal);
+    }
+}
