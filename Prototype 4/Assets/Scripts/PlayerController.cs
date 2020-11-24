5ca7c2bc2ed21de8ef81e24366766899912e46b1
Anas Rasyid <anasrasyid333@gmail.com>
2020-11-24 11:32:51 +0700

    Powerup Behaviour

--- Prototype 4/Assets/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,10 @@ public class PlayerController : MonoBehaviour
 
     private GameObject focalPoint;
 
+    public bool hasPowerUp = false;
+    private float powerUpStrength = 10;
+    public GameObject powerupIndicator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,5 +26,38 @@ public class PlayerController : MonoBehaviour
         float forwadInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(focalPoint.transform.forward * forwadInput * speed);
+        powerupIndicator.transform.position = transform.position + Vector3.up * -0.5f;
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        powerupIndicator.gameObject.SetActive(true);
+        yield return new WaitForSeconds(7);
+        hasPowerUp = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position 
+                - transform.position;
+
+            enemyRb.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+            Debug.Log("Colided with : " + collision.gameObject.name
+                + " with powerup set to " + hasPowerUp);
+        }
     }
 }
